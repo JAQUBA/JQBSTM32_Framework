557c6d77f4581cf45a0c6a1aeaf732cfb5a36eaf
JAQUBA <kjakubowski0492@gmail.com>
2024-06-13 13:29:17 +0200

    nvram to task

--- Util/NV_RAM/NV_RAM.h
+++ Util/NV_RAM/NV_RAM.h
@@ -8,6 +8,28 @@ class NV_RAM {
         void write16(uint16_t address, uint16_t *data, uint16_t size);
         
     private:
+        I2C_HandleTypeDef *_instance;
+        uint8_t _address;
+        uint8_t _i2c_state;
+        uint16_t _pages;
+        uint16_t _pageSize;
+        uint16_t _offset;
+        uint16_t *_data;
+        uint16_t _size;
+        uint32_t _timeout;
+        uint8_t  _status;
+        
+        enum States {
+            I2C_OK,
+            I2C_ERR_HAL,
+            I2C_ERR_TO_S,
+            I2C_ERR_TO_W,
+            NIL,
+            START_READ16,
+            WAIT_COMPL_READ16,
+            START_WRITE16,
+            WAIT_COMPL_WRITE16
+        } _state;
        
 };
 #endif
