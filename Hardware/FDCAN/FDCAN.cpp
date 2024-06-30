1f0d14a69f11441442b084809af4ef0fbdcf7ff0
JAQUBA <kjakubowski0492@gmail.com>
2024-06-30 12:18:20 +0200

    fdcan fix + makehex script

--- Hardware/FDCAN/FDCAN.cpp
+++ Hardware/FDCAN/FDCAN.cpp
@@ -6,19 +6,23 @@ FDCAN *_FDCAN_instances[FDCAN_MAX_INSTANCES];
 uint8_t _FDCAN_instancesNum;
 
 FDCAN::FDCAN(FDCAN_HandleTypeDef *pHandler) {
-    _pHandler = pHandler;
+    _pInstance = pHandler;
     _FDCAN_instances[_FDCAN_instancesNum++] = this;
+    addTaskMain(taskCallback {
+        poll();
+    }, 0);
+    init();
 }
 void FDCAN::init() {
     sFilterConfig.IdType = FDCAN_EXTENDED_ID;
 	sFilterConfig.FilterType = FDCAN_FILTER_MASK;
 	sFilterConfig.FilterID1 = 0;
 	sFilterConfig.FilterID2 = 0;
-	HAL_FDCAN_ConfigFilter(_pHandler, &sFilterConfig);
-	HAL_FDCAN_ConfigRxFifoOverwrite(_pHandler, FDCAN_RX_FIFO0, FDCAN_RX_FIFO_OVERWRITE);
-	HAL_FDCAN_ConfigRxFifoOverwrite(_pHandler, FDCAN_RX_FIFO1, FDCAN_RX_FIFO_OVERWRITE);
-	HAL_FDCAN_Start(_pHandler);
-	HAL_FDCAN_ActivateNotification(_pHandler, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
+	HAL_FDCAN_ConfigFilter(_pInstance, &sFilterConfig);
+	HAL_FDCAN_ConfigRxFifoOverwrite(_pInstance, FDCAN_RX_FIFO0, FDCAN_RX_FIFO_OVERWRITE);
+	HAL_FDCAN_ConfigRxFifoOverwrite(_pInstance, FDCAN_RX_FIFO1, FDCAN_RX_FIFO_OVERWRITE);
+	HAL_FDCAN_Start(_pInstance);
+	HAL_FDCAN_ActivateNotification(_pInstance, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
 
 
     pTxHeader.IdType = FDCAN_EXTENDED_ID;
@@ -35,17 +39,17 @@ void FDCAN::init() {
 void FDCAN::send(uint32_t identifier, uint8_t *data, uint16_t dataLen, uint32_t DataLength) {
     pTxHeader.Identifier = identifier;
     pTxHeader.DataLength = DataLength;
-    HAL_FDCAN_AddMessageToTxFifoQ(_pHandler, &pTxHeader, data);
+    HAL_FDCAN_AddMessageToTxFifoQ(_pInstance, &pTxHeader, data);
 }
 
 FDCAN *FDCAN::getInstance(FDCAN_HandleTypeDef *_instance) {
     for (size_t i = 0; i < _FDCAN_instancesNum; i++) {
-        if(_FDCAN_instances[i]->_pHandler->Instance == _instance->Instance) return _FDCAN_instances[i];
+        if(_FDCAN_instances[i]->_pInstance->Instance == _instance->Instance) return _FDCAN_instances[i];
     }
     return nullptr;
 }
 void FDCAN::RxFifo0Callback() {
-    if(HAL_FDCAN_GetRxMessage(_pHandler, FDCAN_RX_FIFO0, &pRxHeader, recv_buff) != HAL_OK) return;
+    if(HAL_FDCAN_GetRxMessage(_pInstance, FDCAN_RX_FIFO0, &pRxHeader, recv_buff) != HAL_OK) return;
     hasPacket = true;
 }
 void FDCAN::onPacket(uint16_t commNumber, void(*handler)(uint8_t *data, uint16_t dataLen)) {
