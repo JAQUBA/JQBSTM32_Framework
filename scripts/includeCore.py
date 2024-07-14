ac1585016623b1db1ae4c0bfc647d7d24d308991
JAQUBA <kjakubowski0492@gmail.com>
2024-07-14 17:23:40 +0200

    WIP

--- scripts/includeCore.py
+++ scripts/includeCore.py
@@ -1,25 +1,26 @@
 import os
 Import("env")
-env.BuildSources(
-    os.path.join("$BUILD_DIR", "Core"),
-    os.path.join("$PROJECT_DIR", "Core", "Src")
-)
+
 env.Append(CPPPATH=["$PROJECT_DIR/Core/Inc"])
+# env.BuildSources(
+#     os.path.join("$BUILD_DIR", "Core", "Src"),
+#     os.path.join("$PROJECT_DIR", "Core", "Src")
+# )
 
-# from os.path import realpath
+for item in env.GetProjectConfig().parse_multi_values(env.GetProjectOption("using", "")):
+    if item == "FDCAN":
+        env.Append(CPPDEFINES=["_JQB_USE_FDCAN"])
+    if item == "USB_CDC":
+        env.Append(CPPDEFINES=["_JQB_USE_USB_CDC"])
 
-# for item in env.GetProjectConfig().parse_multi_values(env.GetProjectOption("using")):
-#     if item == "FDCAN":
-#         env.Prepend(CPPDEFINES=["_JQB_USE_FDCAN"])
-#     if item == "USB_CDC":
-#         env.Prepend(CPPDEFINES=["_JQB_USE_USB_CDC"])
-#         env.Append(CPPPATH=["USB_Device/App"])
-#         env.Append(CPPPATH=["USB_Device/Target"])
-#         env.BuildSources(
-#             os.path.join("$BUILD_DIR", "USB_Device", "App"),
-#             os.path.join("$PROJECT_DIR", "USB_Device", "App")
-#         )
-#         env.BuildSources(
-#             os.path.join("$BUILD_DIR", "USB_Device", "Target"),
-#             os.path.join("$PROJECT_DIR", "USB_Device", "Target")
-#         )
+        env.Append(CPPPATH=["$PROJECT_DIR/USB_Device/App"])
+        env.Append(CPPPATH=["$PROJECT_DIR/USB_Device/Target"])
+        
+        # env.BuildSources(
+        #     os.path.join("$BUILD_DIR", "USB_Device", "App"),
+        #     os.path.join("$PROJECT_DIR", "USB_Device", "App")
+        # )
+        # env.BuildSources(
+        #     os.path.join("$BUILD_DIR", "USB_Device", "Target"),
+        #     os.path.join("$PROJECT_DIR", "USB_Device", "Target")
+        # )
