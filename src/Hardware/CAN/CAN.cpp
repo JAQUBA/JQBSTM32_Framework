d9fd47ec2288435f7cd7e5aa0218adb0435a319d
JAQUBA <JAQUBA@users.noreply.github.com>
2024-07-26 21:56:49 +0200

    can init

---
+++ src/Hardware/CAN/CAN.cpp
@@ -0,0 +1,7 @@
+#include "CAN.h"
+
+#include "can.h"
+
+CAN::CAN() {
+    // HAL_CAN_ConfigFilter(_pInstance, &sFilterConfig);
+}
---
+++ src/Hardware/CAN/CAN.h
@@ -0,0 +1,10 @@
+#ifndef _CAN_H
+#define _CAN_H
+#include "Core.h"
+
+class CAN {
+    public:
+        CAN();
+};
+
+#endif
