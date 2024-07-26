562d11220a209b4cf547e786001f3e50088b3e3a
Jan Bator <janbator2@gmail.com>
2024-07-26 21:38:28 +0200

    can dodany

--- src/Hardware/CAN/CAN.h
+++ src/Hardware/CAN/CAN.h
@@ -5,15 +5,15 @@
 #include "Interface/IBus.h"
 #include <unordered_map> 
 
-#define FDCAN_MAX_INSTANCES 1
+#define CAN_MAX_INSTANCES 1
 
-class FDCAN : public IBus{
+class CAN : public IBus{
 	public:
-		static FDCAN *getInstance(CAN_HandleTypeDef *pHandler);
-		FDCAN(CAN_HandleTypeDef *pHandler);
+		static CAN *getInstance(CAN_HandleTypeDef *pHandler);
+		CAN(CAN_HandleTypeDef *pHandler);
 		
 		void onPacket(uint16_t commNumber, dataCallback_f handler);
-		void send(uint32_t identifier, uint8_t *pData, uint16_t Size, uint32_t DataLength = FDCAN_DLC_BYTES_8);
+		void send(uint32_t identifier, uint8_t *pData, uint16_t Size, uint32_t DataLength = 0);//FDCAN_DLC_BYTES_8);
 
         void rxInterrupt();
 
@@ -24,6 +24,7 @@ class FDCAN : public IBus{
 		CAN_RxHeaderTypeDef pRxHeader;
 		CAN_TxHeaderTypeDef pTxHeader;
 
+		uint32_t canMailbox;
 		uint8_t pData[8] = {0,0,0,0,0,0,0,0};
 		bool hasPacket = false;
 
