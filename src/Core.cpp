f4f442c5305472ce05a7e39a84df03bd56743464
JAQUBA <JAQUBA@users.noreply.github.com>
2024-07-31 17:41:49 +0200

    core tab format

--- src/Core.cpp
+++ src/Core.cpp
@@ -8,20 +8,20 @@ void addTaskMain(taskCallback_f functionPointer, uint32_t delay, bool single) {m
 
 #ifdef DWT
 uint8_t DWT_COUNTER_ENABLE(void) {
-  CoreDebug->DEMCR &= ~0x01000000;
-  CoreDebug->DEMCR |=  0x01000000;
-  DWT->CTRL &= ~0x00000001;
-  DWT->CTRL |=  0x00000001;
-  DWT->CYCCNT = 0;
-  uint32_t c = DWT->CYCCNT;
-  __ASM volatile ("NOP"); __ASM volatile ("NOP"); __ASM volatile ("NOP");
-  if((DWT->CYCCNT - c) == 0){ return 0; }
-  return (DWT->CYCCNT - c);
+	CoreDebug->DEMCR &= ~0x01000000;
+	CoreDebug->DEMCR |=  0x01000000;
+	DWT->CTRL &= ~0x00000001;
+	DWT->CTRL |=  0x00000001;
+	DWT->CYCCNT = 0;
+	uint32_t c = DWT->CYCCNT;
+	__ASM volatile ("NOP"); __ASM volatile ("NOP"); __ASM volatile ("NOP");
+	if((DWT->CYCCNT - c) == 0){ return 0; }
+	return (DWT->CYCCNT - c);
 }
 void delay_us(volatile uint32_t delay_us) {
-  uint32_t clk_cycle_start = DWT->CYCCNT;
-  delay_us *= (HAL_RCC_GetHCLKFreq() / 1000000);
-  while ((DWT->CYCCNT - clk_cycle_start) < delay_us);
+	uint32_t clk_cycle_start = DWT->CYCCNT;
+	delay_us *= (HAL_RCC_GetHCLKFreq() / 1000000);
+	while ((DWT->CYCCNT - clk_cycle_start) < delay_us);
 }
 #endif
 void delay(volatile uint32_t delay_ms) {HAL_Delay(delay_ms);}
@@ -29,9 +29,9 @@ uint32_t ulMillis;
 uint32_t millis() {return ulMillis;}
 
 Core::Core() {
-  HAL_Init();
+	HAL_Init();
 	SystemClock_Config();
-  init();
+	init();
 	HAL_TIM_Base_Start_IT(&TASK_TIMER);
 #ifdef DWT
 	DWT_COUNTER_ENABLE();
@@ -43,9 +43,9 @@ int main() {
 	setup();
 
 	while (1) {
-        mainTasks.execute();
-        loop();
-    }
+		mainTasks.execute();
+		loop();
+	}
 }
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	if (htim == &TASK_TIMER) {
@@ -56,7 +56,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	}
 }
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 __weak void init() {}
