19034103b4e461872d9958007b14ba2a321775a0
JAQUBA <JAQUBA@users.noreply.github.com>
2024-08-04 21:53:13 +0200

    first try

--- src/Hardware/Analog/Analog.h
+++ src/Hardware/Analog/Analog.h
@@ -3,18 +3,48 @@
 
 #include "../../Core.h"
 
+/**
+ * @class Analog
+ * @brief Klasa do obsługi analogowych wejść.
+ */
 class Analog {
     public:
+        /**
+         * @brief Inicjalizuje ADC.
+         * @param pHandler Wskaźnik do struktury ADC_HandleTypeDef.
+         */
         static void init(ADC_HandleTypeDef *pHandler);
+
+        /**
+         * @brief Konstruktor klasy Analog.
+         * @param channelNumber Numer kanału ADC.
+         */
         Analog(uint8_t channelNumber);
+
+        /**
+         * @brief Pobiera surową wartość ADC.
+         * @return Surowa wartość ADC.
+         */
         uint16_t getRaw();
+
+        /**
+         * @brief Pobiera przetworzoną wartość ADC.
+         * @return Przetworzona wartość ADC.
+         */
         uint16_t getValue();
+
+        /**
+         * @brief Konfiguruje kanał ADC.
+         * @param offset Wskaźnik do offsetu.
+         * @param divider Wskaźnik do dzielnika.
+         */
         void configureChannel(uint16_t *offset, uint16_t *divider);
+
     private:
-        static uint16_t rawADC[8];
-        uint8_t _channelNumber;
-        uint16_t *_offset;
-        uint16_t *_divider;
+        static uint16_t rawADC[8]; /**< Tablica surowych wartości ADC. */
+        uint8_t _channelNumber; /**< Numer kanału ADC. */
+        uint16_t *_offset; /**< Wskaźnik do offsetu. */
+        uint16_t *_divider; /**< Wskaźnik do dzielnika. */
 };
 
 #endif
