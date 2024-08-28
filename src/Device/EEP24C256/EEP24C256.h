46a24c2686d40c29813aa2b9d7a98962e547698e
Jan Bator <janbator2@gmail.com>
2024-08-28 12:18:05 +0200

    teraz działa

---
+++ src/Device/EEP24C256/EEP24C256.h
@@ -0,0 +1,31 @@
+#ifndef __EEP24C256_H_
+#define __EEP24C256_H_
+#include "../../Core.h"
+#include "../../Hardware/I2C/I2C.h"
+#include "../../Interface/IExternalMemory.h"
+
+class EEP24C256 : public IExternalMemory {
+    public:
+        EEP24C256(I2C *pInstance,
+            uint16_t DevAddress = 0xA0,
+            uint32_t BaseAddress = 0x00
+        );
+        void readFromMemory(
+            uint32_t MemAddress,
+            uint8_t *pData, 
+            uint16_t Size
+        ) override;
+
+        void writeToMemory(
+            uint32_t MemAddress,
+            uint8_t *pData, 
+            uint16_t Size
+        ) override;
+        
+    protected:
+        I2C             *_pInstance;
+        uint16_t        _DevAddress;
+        uint32_t        _BaseAddress;
+        uint16_t        _MemAddSize = ADDRESS_SIZE_16BIT;
+};
+#endif
