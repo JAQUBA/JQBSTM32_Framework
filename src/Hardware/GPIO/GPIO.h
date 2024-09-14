842186f7e5ed525eaec0246d147d454786d9726a
JAQUBA <JAQUBA@users.noreply.github.com>
2024-09-14 22:51:57 +0200

    interrupts to test

--- src/Hardware/GPIO/GPIO.h
+++ src/Hardware/GPIO/GPIO.h
@@ -8,6 +8,12 @@
 
 class HardwareGPIO {
   public:
+
+    enum InterruptMode {
+        RISING = 0,
+        FALLING = 1,
+        RISING_FALLING = 2
+    };
     HardwareGPIO();
     void setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode);
     void setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode, uint32_t pullup);
@@ -17,6 +23,16 @@ class HardwareGPIO {
     GPIO_PinState readInput(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
     GPIO_PinState readOutput(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 
+    void attachInterrupt(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, voidCallback_f callback, InterruptMode mode);
+    void _interruptCallback(uint16_t GPIO_Pin);
+
+    private:
+      struct interrupt {
+        GPIO_TypeDef* GPIOx;
+        uint16_t GPIO_Pin;
+        voidCallback_f callback;
+      };
+      std::list<interrupt> interrupts;
 };
 
 extern HardwareGPIO GPIO;
