608c89a1620977041dc7e9ecca442af8ae66e096
JAQUBA <kjakubowski0492@gmail.com>
2024-10-03 11:16:20 +0200

    add working interrupts

--- src/Hardware/GPIO/GPIO.cpp
+++ src/Hardware/GPIO/GPIO.cpp
@@ -3,39 +3,16 @@
 HardwareGPIO GPIO;
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {GPIO._interruptCallback(GPIO_Pin);}
-
-HardwareGPIO::HardwareGPIO() {
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+void HardwareGPIO::_interruptCallback(uint16_t GPIO_Pin) {
+    for (auto interrupt : interrupts) {if(interrupt.GPIO_Pin == GPIO_Pin) interrupt.callback();}
 }
-
-void HardwareGPIO::attachInterrupt(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, voidCallback_f callback, uint32_t mode) {
-    GPIO_InitTypeDef GPIO_InitStruct  = {0};
-
-    GPIO_InitStruct.Pin = GPIO_Pin;
-    GPIO_InitStruct.Mode = mode;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
-
-    HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI0_IRQn);
-
+void HardwareGPIO::attachInterrupt(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, voidCallback_f callback) {
     interrupt interrupt;
     interrupt.GPIOx = GPIOx;
     interrupt.GPIO_Pin = GPIO_Pin;
     interrupt.callback = callback;
     interrupts.push_back(interrupt);
 }
-void HardwareGPIO::_interruptCallback(uint16_t GPIO_Pin) {
-    for (auto interrupt : interrupts) {
-        if (interrupt.GPIO_Pin == GPIO_Pin) {
-            interrupt.callback();
-        }
-    }
-}
-
 void HardwareGPIO::setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode) {
     GPIO_InitTypeDef GPIO_InitStruct  = {0};
 
@@ -54,15 +31,6 @@ void HardwareGPIO::setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode, 
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
 }
-// void HardwareGPIO::setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode, uint32_t pullup, uint32_t alternate) {
-//     GPIO_InitTypeDef GPIO_InitStruct  = {0};
-//     GPIO_InitStruct.Pin = GPIO_Pin;
-//     GPIO_InitStruct.Mode = mode;
-//     GPIO_InitStruct.Pull = pullup;
-//     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-//     GPIO_InitStruct.Alternate = alternate;
-//     HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
-// }
 
 void HardwareGPIO::set(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) {
     HAL_GPIO_WritePin(GPIOx, GPIO_Pin, PinState);
--- src/Hardware/GPIO/GPIO.h
+++ src/Hardware/GPIO/GPIO.h
@@ -8,16 +8,14 @@
 
 class HardwareGPIO {
   public:
-    HardwareGPIO();
     void setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode);
     void setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode, uint32_t pullup);
-    // void setup(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t mode, uint32_t pullup, uint32_t alternate);
     void set(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
     void toggle(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
     GPIO_PinState readInput(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
     GPIO_PinState readOutput(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 
-    void attachInterrupt(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, voidCallback_f callback, uint32_t mode);
+    void attachInterrupt(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, voidCallback_f callback);
     void _interruptCallback(uint16_t GPIO_Pin);
 
     private:
