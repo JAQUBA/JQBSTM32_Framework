dfc20c738145993caea5b421cc152a3608dcfcc0
Jan Bator <janbator2@gmail.com>
2024-11-18 19:50:14 +0100

    ds zaczyna działać

--- src/Device/DS18B20/DS18B20.cpp
+++ src/Device/DS18B20/DS18B20.cpp
@@ -1,41 +1,46 @@
 #include "DS18B20.h"
 #ifdef __DS18B20_H_
 
+uint64_t sensors[DS_MAX_SENSORS] = {0};
+
 bool readT = false;
 
-DS18B20::DS18B20(OneWire *oneWire) {
+void unpack_rom(uint64_t number, uint8_t *result) {
+    result[7] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[6] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[5] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[4] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[3] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[2] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[1] = number & 0x00000000000000FF ; number = number >> 8 ;
+    result[0] = number & 0x00000000000000FF ;
+}
 
-    this->oneWire = oneWire;
-
+DS18B20::DS18B20(OneWire *a_oneWire) {
+    this->oneWire = a_oneWire;
     addTaskMain(taskCallback {
-        if (!sensors.empty()) {
-            if (readT) {
-            
-                currentSensor = sensors.front();
-                oneWire->sesja(0x55, currentSensor.pDataRom,
-                0xBE, 0, 0, currentSensor.rd, 9); //odczyt temperatury
-                readT = false;
-		    } else {
-		        readT = true;
-			oneWire->sesja(0xCC, 0, 0x44);//start konwer. temp.
-		    }
+        if (readT) {
+            for (id=0; id<DS_MAX_SENSORS; id++) {
+                unpack_rom(sensors[id], b_rom);
+                if (b_rom[id]>0) {
+                    oneWire->sesja(0x55, b_rom,
+                    0xBE, 0, 0, b_rd+(id*9), 9); //odczyt temperatury
+                }
+            }
+            readT = false;
+        } else {
+            readT = true;
+            oneWire->sesja(0xCC, 0, 0x44);//start konwer. temp.
         }
 	}, 1000);
 }
 
-void DS18B20::addSensor(uint8_t *romCode, uint8_t nr) {
-
-    sensor sensor;
-
-    sensor.pDataRom = romCode;
-    sensor.nr = nr;
-    sensors.push(sensor);
+void DS18B20::addSensor(uint64_t romCode, uint8_t id) {
+    sensors[id]=romCode;
 }
 
 uint16_t DS18B20::getTemperature(uint8_t id) {
-
-    return ((*(currentSensor.rd+1)) << 8) | (*(currentSensor.rd));
-    
-   // return (recv[1] << 8) | recv[0];
+    return    (*(b_rd+(id*9)+1))<<8 | *(b_rd+(id*9));
 }
+
 #endif // __DS18B20_H__
--- src/Device/DS18B20/DS18B20.h
+++ src/Device/DS18B20/DS18B20.h
@@ -4,25 +4,20 @@
 #ifndef __DS18B20_H_
 #define __DS18B20_H_
 
+#ifndef DS_MAX_SENSORS
+#define DS_MAX_SENSORS 10
+#endif
+
 class DS18B20 {
     public:
         DS18B20(OneWire *oneWire);
-        void addSensor(uint8_t *romCode, uint8_t nr);
+        void addSensor(uint64_t romCode, uint8_t nr);
         uint16_t getTemperature(uint8_t id);
     private:
         OneWire *oneWire;
-
-        
-
-        struct sensor {
-           // EoperationType  operationType;
-            uint8_t         *pDataRom;
-            uint8_t         nr;
-            uint8_t         rd[9]={0};
-            //dataCallback_f  callback_f = nullptr;
-        } currentSensor;
-        
-        std::queue<sensor> sensors;
+        uint8_t b_rom[8] ={0};
+        uint8_t id=0;
+        uint8_t b_rd[DS_MAX_SENSORS * 9]={0};
 };
 
 #endif // __DS18B20_H__
