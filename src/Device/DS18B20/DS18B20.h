21afc3323f26e47cbdfe777ed947ed95021e4818
JAQUBA <JAQUBA@users.noreply.github.com>
2024-11-29 09:07:37 +0100

    Development (#27)
    * ds 1
    
    * po 1sek zwiecha
    
    * ds zaczyna działać
    
    * ds dodany read_rom
    
    * ow dodane enum
    
    ---------
    
    Co-authored-by: Jan Bator <janbator2@gmail.com>

--- src/Device/DS18B20/DS18B20.h
+++ src/Device/DS18B20/DS18B20.h
@@ -1,15 +1,27 @@
 #include "../../Hardware/OneWire/OneWire.h"
-#ifdef __ONEWIRE_H_
+#ifdef ONEWIRE_H
 
 #ifndef __DS18B20_H_
 #define __DS18B20_H_
 
+#ifndef DS_MAX_SENSORS
+#define DS_MAX_SENSORS 10
+#endif
+
 class DS18B20 {
     public:
         DS18B20(OneWire *oneWire);
-        uint16_t getTemperature(uint64_t romCode);
+        void addSensor(uint64_t romCode, uint8_t nr);
+        uint16_t getTemperature(uint8_t id);
+        void readRom(void);
+        uint64_t getRom(void);
     private:
+        bool read_rom = false;
         OneWire *oneWire;
+        uint8_t b_rom[8] ={0};
+        uint64_t rom;
+        uint8_t id=0;
+        uint8_t b_rd[DS_MAX_SENSORS * 9]={0};
 };
 
 #endif // __DS18B20_H__
