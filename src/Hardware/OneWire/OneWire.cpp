03c5eb237143f27c7702899cd818162c6a7c1486
JAQUBA <kjakubowski0492@gmail.com>
2024-12-03 20:24:19 +0100

    to tab

--- src/Hardware/OneWire/OneWire.cpp
+++ src/Hardware/OneWire/OneWire.cpp
@@ -1,238 +1,238 @@
 #include "OneWire.h"
 
 OneWire::OneWire(Timer* timer, GPIO_TypeDef* GPIO_Port, uint16_t GPIO_Pin) : OW_Timer(timer), OW_Port(GPIO_Port), OW_Pin(GPIO_Pin) {
-    OW_Timer->attachInterrupt(Timer::PeriodElapsedCallback, voidCallback {
-        switch(operationProgress) {
-            case OPERATION_PROGRESS_IDLE: {
-                if(operationState == WAITING) operationState = FINISH;
-                break;
-            }
-            case OPERATION_PROGRESS_RESET: {
-                HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_RESET);
-                OW_Timer->setPeriod(480);
-                operationProgress = OPERATION_PROGRESS_RESET_WAIT_LOW;
-                break;
-            }
-            case OPERATION_PROGRESS_RESET_WAIT_LOW: {
-                HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_SET);
-                OW_Timer->setPeriod(70);
-                operationProgress=OPERATION_PROGRESS_RESET_WAIT_HIGH;
-                break;
-            }
-            case OPERATION_PROGRESS_RESET_WAIT_HIGH: {
-                is_device_presence = HAL_GPIO_ReadPin(OW_Port, OW_Pin);
-                OW_Timer->setPeriod(410);
-                operationProgress=OPERATION_PROGRESS_IDLE;
-                break;
-            }
+	OW_Timer->attachInterrupt(Timer::PeriodElapsedCallback, voidCallback {
+		switch(operationProgress) {
+			case OPERATION_PROGRESS_IDLE: {
+				if(operationState == WAITING) operationState = FINISH;
+				break;
+			}
+			case OPERATION_PROGRESS_RESET: {
+				HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_RESET);
+				OW_Timer->setPeriod(480);
+				operationProgress = OPERATION_PROGRESS_RESET_WAIT_LOW;
+				break;
+			}
+			case OPERATION_PROGRESS_RESET_WAIT_LOW: {
+				HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_SET);
+				OW_Timer->setPeriod(70);
+				operationProgress=OPERATION_PROGRESS_RESET_WAIT_HIGH;
+				break;
+			}
+			case OPERATION_PROGRESS_RESET_WAIT_HIGH: {
+				is_device_presence = HAL_GPIO_ReadPin(OW_Port, OW_Pin);
+				OW_Timer->setPeriod(410);
+				operationProgress=OPERATION_PROGRESS_IDLE;
+				break;
+			}
 
-            case OPERATION_PROGRESS_WRITE_START: {
-                ow_byte = *(currentOperation.pData + ow_byte_index);
-                ow_bit_index = 0;
-            }
-            case OPERATION_PROGRESS_WRITE_BIT: {
-                HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_RESET);
-                OW_Timer->setPeriod(ow_byte & 0x01?6:60);
-                operationProgress=OPERATION_PROGRESS_WRITE_END;
-                break;
-            }
-            case OPERATION_PROGRESS_WRITE_END: {
-                HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_SET);
-                OW_Timer->setPeriod(ow_byte & 0x01?64:10);
-                if (++ow_bit_index < 8) {
-                    ow_byte >>= 1;
-                    operationProgress = OPERATION_PROGRESS_WRITE_BIT;
-                } else {
-                    if (++ow_byte_index<currentOperation.Size) {
-                        operationProgress = OPERATION_PROGRESS_WRITE_START;
-                        break;
-                    }
-                    operationProgress = OPERATION_PROGRESS_IDLE;
-                }
-                break;
-            }
+			case OPERATION_PROGRESS_WRITE_START: {
+				ow_byte = *(currentOperation.pData + ow_byte_index);
+				ow_bit_index = 0;
+			}
+			case OPERATION_PROGRESS_WRITE_BIT: {
+				HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_RESET);
+				OW_Timer->setPeriod(ow_byte & 0x01?6:60);
+				operationProgress=OPERATION_PROGRESS_WRITE_END;
+				break;
+			}
+			case OPERATION_PROGRESS_WRITE_END: {
+				HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_SET);
+				OW_Timer->setPeriod(ow_byte & 0x01?64:10);
+				if (++ow_bit_index < 8) {
+					ow_byte >>= 1;
+					operationProgress = OPERATION_PROGRESS_WRITE_BIT;
+				} else {
+					if (++ow_byte_index<currentOperation.Size) {
+						operationProgress = OPERATION_PROGRESS_WRITE_START;
+						break;
+					}
+					operationProgress = OPERATION_PROGRESS_IDLE;
+				}
+				break;
+			}
 
-            case OPERATION_PROGRESS_READ_START: {
-                ow_bit_index = 0;
-                ow_byte = 0;
-            }
-            case OPERATION_PROGRESS_READ_BIT: {
-                HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_RESET);
-                OW_Timer->setPeriod(10);
-                operationProgress=OPERATION_PROGRESS_READ_WAIT;
-                break;
-            }
-            case OPERATION_PROGRESS_READ_WAIT: {
-                HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_SET);
-                OW_Timer->setPeriod(9);
-                operationProgress=OPERATION_PROGRESS_READ_END;
-                break;
-            }
-            case OPERATION_PROGRESS_READ_END: {
-                if (HAL_GPIO_ReadPin(OW_Port, OW_Pin))	ow_byte |= 0x80;
-                OW_Timer->setPeriod(55);
-                if (++ow_bit_index < 8) {
-                    ow_byte >>= 1;
-                    operationProgress = OPERATION_PROGRESS_READ_BIT;
-                } else {
-                    *(currentOperation.pData + ow_byte_index) = ow_byte;
-                    if(++ow_byte_index<currentOperation.Size) {
-                        operationProgress = OPERATION_PROGRESS_READ_START;
-                        break;
-                    }
-                    operationProgress = OPERATION_PROGRESS_IDLE;
-                }
-                break;
-            }
-        }
-    });
+			case OPERATION_PROGRESS_READ_START: {
+				ow_bit_index = 0;
+				ow_byte = 0;
+			}
+			case OPERATION_PROGRESS_READ_BIT: {
+				HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_RESET);
+				OW_Timer->setPeriod(10);
+				operationProgress=OPERATION_PROGRESS_READ_WAIT;
+				break;
+			}
+			case OPERATION_PROGRESS_READ_WAIT: {
+				HAL_GPIO_WritePin(OW_Port, OW_Pin, GPIO_PIN_SET);
+				OW_Timer->setPeriod(9);
+				operationProgress=OPERATION_PROGRESS_READ_END;
+				break;
+			}
+			case OPERATION_PROGRESS_READ_END: {
+				if (HAL_GPIO_ReadPin(OW_Port, OW_Pin))	ow_byte |= 0x80;
+				OW_Timer->setPeriod(55);
+				if (++ow_bit_index < 8) {
+					ow_byte >>= 1;
+					operationProgress = OPERATION_PROGRESS_READ_BIT;
+				} else {
+					*(currentOperation.pData + ow_byte_index) = ow_byte;
+					if(++ow_byte_index<currentOperation.Size) {
+						operationProgress = OPERATION_PROGRESS_READ_START;
+						break;
+					}
+					operationProgress = OPERATION_PROGRESS_IDLE;
+				}
+				break;
+			}
+		}
+	});
 
-    addTaskMain(taskCallback {
-        switch(operationState) {
+	addTaskMain(taskCallback {
+		switch(operationState) {
 			case IDLE: {
 				if(!operations.empty()) {
 					currentOperation = operations.front();
 					operationState = CHECK_FREE;
-                }
-			    break;
-            }
+				}
+				break;
+			}
 			case CHECK_FREE: {
 				if(operationProgress == OPERATION_PROGRESS_IDLE) {
 					operationState = WORK;
-                }
-                break;
-            }
+				}
+				break;
+			}
 			case WORK: {
 				operationTimeout = millis()+40;
-                if(currentOperation.operationType == EoperationType::RESET) {
-                    is_device_presence = false;
-                    operationProgress = OPERATION_PROGRESS_RESET;
+				if(currentOperation.operationType == EoperationType::RESET) {
+					is_device_presence = false;
+					operationProgress = OPERATION_PROGRESS_RESET;
 				} else if(currentOperation.operationType == EoperationType::TRANSMIT) {
-                    ow_byte_index = 0;
-                    operationProgress = OPERATION_PROGRESS_WRITE_START;
+					ow_byte_index = 0;
+					operationProgress = OPERATION_PROGRESS_WRITE_START;
 
 				} else if(currentOperation.operationType == EoperationType::RECEIVE) {
-                    ow_byte_index = 0;
-                    operationProgress = OPERATION_PROGRESS_READ_START;
+					ow_byte_index = 0;
+					operationProgress = OPERATION_PROGRESS_READ_START;
 				}
-                operationState = WAITING;
-			    break;
-            }
+				operationState = WAITING;
+				break;
+			}
 			case WAITING: {
-       			if (millis() >= operationTimeout) {
+	   			if (millis() >= operationTimeout) {
 					operationState = FINISH;
-                }
-                break;
-            }
+				}
+				break;
+			}
 			case FINISH: {
-            	if(currentOperation.callback_f != nullptr) {
+				if(currentOperation.callback_f != nullptr) {
 					currentOperation.callback_f(
 						currentOperation.pData,
 						currentOperation.Size
 					);
-                }
+				}
 				operationState = CLEAR;
-    		    break;
-            }
+				break;
+			}
 			case CLEAR: {
 				if(currentOperation.free) free(currentOperation.pData);
 				operations.pop();
 				operationState = IDLE;
-                break;
-            }
+				break;
+			}
 		}
-    });
+	});
 }
 
 void OneWire::reset() {
-    operation operation;
-    operation.operationType = EoperationType::RESET;
-    operation.free = false;
-    operations.push(operation);
+	operation operation;
+	operation.operationType = EoperationType::RESET;
+	operation.free = false;
+	operations.push(operation);
 }
 void OneWire::transmit(
-    const uint8_t* pData, uint16_t size,
-    dataCallback_f callbackFn
+	const uint8_t* pData, uint16_t size,
+	dataCallback_f callbackFn
 ){
-    operation operation;
-    operation.operationType = EoperationType::TRANSMIT;
-    operation.pData = (uint8_t*) malloc(size);
-    memcpy(operation.pData, pData, size);
-    operation.Size = size;
-    operation.free = true;
-    operation.callback_f = callbackFn;
-    operations.push(operation);
+	operation operation;
+	operation.operationType = EoperationType::TRANSMIT;
+	operation.pData = (uint8_t*) malloc(size);
+	memcpy(operation.pData, pData, size);
+	operation.Size = size;
+	operation.free = true;
+	operation.callback_f = callbackFn;
+	operations.push(operation);
 }
 
 void OneWire::receive(
-    uint8_t* pData, uint16_t size,
-    dataCallback_f callbackFn
+	uint8_t* pData, uint16_t size,
+	dataCallback_f callbackFn
 ){
-    operation operation;
-    operation.operationType = EoperationType::RECEIVE;
-    operation.pData = pData;
-    operation.Size = size;
-    operation.free = false;
-    operation.callback_f = callbackFn;
-    operations.push(operation);
+	operation operation;
+	operation.operationType = EoperationType::RECEIVE;
+	operation.pData = pData;
+	operation.Size = size;
+	operation.free = false;
+	operation.callback_f = callbackFn;
+	operations.push(operation);
 }
 
 void OneWire::transmitThenReceive(
-    const uint8_t* pData_tx, uint16_t txSize,
-    uint8_t* pData_rx, uint16_t rxSize,
-    dataCallback_f callbackFn
+	const uint8_t* pData_tx, uint16_t txSize,
+	uint8_t* pData_rx, uint16_t rxSize,
+	dataCallback_f callbackFn
 ){
-    operation operation;
-    operation.operationType = EoperationType::TRANSMIT;
-    operation.pData = (uint8_t*) malloc(txSize);
-    memcpy(operation.pData, pData_tx, txSize);
-    operation.Size = txSize;
-    operation.free = true;
-    operations.push(operation);//transmit
-    receive(pData_rx, rxSize, callbackFn);
+	operation operation;
+	operation.operationType = EoperationType::TRANSMIT;
+	operation.pData = (uint8_t*) malloc(txSize);
+	memcpy(operation.pData, pData_tx, txSize);
+	operation.Size = txSize;
+	operation.free = true;
+	operations.push(operation);//transmit
+	receive(pData_rx, rxSize, callbackFn);
 }
 
 void OneWire::transaction(
-    uint8_t romCommand,
-    const uint8_t* address,
-    uint8_t functionCommand,
-    const uint8_t* pData_tx,
-    uint16_t txSize,
-    uint8_t* pData_rx,
-    uint16_t rxSize,
-    dataCallback_f callbackFn,
-    bool resetAfterTransaction
+	uint8_t romCommand,
+	const uint8_t* address,
+	uint8_t functionCommand,
+	const uint8_t* pData_tx,
+	uint16_t txSize,
+	uint8_t* pData_rx,
+	uint16_t rxSize,
+	dataCallback_f callbackFn,
+	bool resetAfterTransaction
 ){
-    uint8_t Size;
-    operation operation;
+	uint8_t Size;
+	operation operation;
 
-    reset();
+	reset();
 
-    operation.operationType = EoperationType::TRANSMIT;
-    if (address==NULL){
-        Size = 2;
-        if (txSize>0) Size += txSize;
-        operation.pData = (uint8_t*) malloc(Size);
-        *(operation.pData+0) = romCommand;
-        *(operation.pData+1) = functionCommand;
-        if (txSize>0) memcpy(operation.pData+Size, pData_tx, txSize);
-    } else {
-        Size=10;
-        if (txSize>0) Size += txSize;
-        operation.pData = (uint8_t*) malloc(Size);
-        *(operation.pData+0) = romCommand;
-        memcpy(operation.pData+1, address, 8);
-        *(operation.pData+9) = functionCommand;
-        if (txSize>0) memcpy(operation.pData+Size, pData_tx, txSize);
-    }
-    operation.Size = Size;
-    operation.free = true;
-    operations.push(operation);
+	operation.operationType = EoperationType::TRANSMIT;
+	if (address==NULL){
+		Size = 2;
+		if (txSize>0) Size += txSize;
+		operation.pData = (uint8_t*) malloc(Size);
+		*(operation.pData+0) = romCommand;
+		*(operation.pData+1) = functionCommand;
+		if (txSize>0) memcpy(operation.pData+Size, pData_tx, txSize);
+	} else {
+		Size=10;
+		if (txSize>0) Size += txSize;
+		operation.pData = (uint8_t*) malloc(Size);
+		*(operation.pData+0) = romCommand;
+		memcpy(operation.pData+1, address, 8);
+		*(operation.pData+9) = functionCommand;
+		if (txSize>0) memcpy(operation.pData+Size, pData_tx, txSize);
+	}
+	operation.Size = Size;
+	operation.free = true;
+	operations.push(operation);
 
-    if (rxSize>0) receive(pData_rx, rxSize, callbackFn);
-    if (resetAfterTransaction) reset();
+	if (rxSize>0) receive(pData_rx, rxSize, callbackFn);
+	if (resetAfterTransaction) reset();
 }       
 
 uint16_t OneWire::queueSize() {
-    return operations.size();
+	return operations.size();
 }
--- src/Hardware/OneWire/OneWire.h
+++ src/Hardware/OneWire/OneWire.h
@@ -6,98 +6,98 @@
 #include "Hardware/Timer/Timer.h"
 
 class OneWire : public IBus {
-    public:
-        OneWire(Timer* timer, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+	public:
+		OneWire(Timer* timer, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 
-        // Resetuje magistralę 1-Wire
-        void reset();
+		// Resetuje magistralę 1-Wire
+		void reset();
 
-        // Wysyła dane przez 1-Wire
-        void transmit(
-            const uint8_t* pData, uint16_t size,
-            dataCallback_f callbackFn = nullptr
-        );
+		// Wysyła dane przez 1-Wire
+		void transmit(
+			const uint8_t* pData, uint16_t size,
+			dataCallback_f callbackFn = nullptr
+		);
 
-        // Odbiera dane przez 1-Wire
-        void receive(
-            uint8_t* pData, uint16_t size,
-            dataCallback_f callbackFn = nullptr
-        );
+		// Odbiera dane przez 1-Wire
+		void receive(
+			uint8_t* pData, uint16_t size,
+			dataCallback_f callbackFn = nullptr
+		);
 
-        // Sekwencja: najpierw transmisja, potem odbiór
-        void transmitThenReceive(
-            const uint8_t* pData_tx, uint16_t txSize,
-            uint8_t* pData_rx, uint16_t rxSize,
-            dataCallback_f callbackFn = nullptr
-        );
+		// Sekwencja: najpierw transmisja, potem odbiór
+		void transmitThenReceive(
+			const uint8_t* pData_tx, uint16_t txSize,
+			uint8_t* pData_rx, uint16_t rxSize,
+			dataCallback_f callbackFn = nullptr
+		);
 
-        // Pełna transakcja na magistrali 1-Wire
-        void transaction(
-            uint8_t romCommand,
-            const uint8_t* address,
-            uint8_t functionCommand,
-            const uint8_t* pData_tx = nullptr,
-            uint16_t txSize = 0,
-            uint8_t* pData_rx = nullptr,
-            uint16_t rxSize = 0,
-            dataCallback_f callbackFn = nullptr,
-            bool resetAfterTransaction = false
-        );
-        uint16_t queueSize();
+		// Pełna transakcja na magistrali 1-Wire
+		void transaction(
+			uint8_t romCommand,
+			const uint8_t* address,
+			uint8_t functionCommand,
+			const uint8_t* pData_tx = nullptr,
+			uint16_t txSize = 0,
+			uint8_t* pData_rx = nullptr,
+			uint16_t rxSize = 0,
+			dataCallback_f callbackFn = nullptr,
+			bool resetAfterTransaction = false
+		);
+		uint16_t queueSize();
 
-    private:
-        Timer* OW_Timer;
-        GPIO_TypeDef* OW_Port;
-        uint16_t OW_Pin; 
+	private:
+		Timer* OW_Timer;
+		GPIO_TypeDef* OW_Port;
+		uint16_t OW_Pin; 
 
-        uint32_t operationTimeout;
-        bool     is_device_presence;
+		uint32_t operationTimeout;
+		bool     is_device_presence;
 
-        uint8_t  ow_byte;
-        uint8_t  ow_byte_index;
-        uint8_t  ow_bit_index;
-        
-        enum {
-            OPERATION_PROGRESS_IDLE,
+		uint8_t  ow_byte;
+		uint8_t  ow_byte_index;
+		uint8_t  ow_bit_index;
+		
+		enum {
+			OPERATION_PROGRESS_IDLE,
 
-            OPERATION_PROGRESS_RESET,
-            OPERATION_PROGRESS_RESET_WAIT_LOW,
-            OPERATION_PROGRESS_RESET_WAIT_HIGH,
+			OPERATION_PROGRESS_RESET,
+			OPERATION_PROGRESS_RESET_WAIT_LOW,
+			OPERATION_PROGRESS_RESET_WAIT_HIGH,
 
-            OPERATION_PROGRESS_WRITE_START,
-            OPERATION_PROGRESS_WRITE_BIT,
-            OPERATION_PROGRESS_WRITE_END,
+			OPERATION_PROGRESS_WRITE_START,
+			OPERATION_PROGRESS_WRITE_BIT,
+			OPERATION_PROGRESS_WRITE_END,
 
-            OPERATION_PROGRESS_READ_START,
-            OPERATION_PROGRESS_READ_BIT,
-            OPERATION_PROGRESS_READ_WAIT,
-            OPERATION_PROGRESS_READ_END
-        } operationProgress = OPERATION_PROGRESS_IDLE;   
+			OPERATION_PROGRESS_READ_START,
+			OPERATION_PROGRESS_READ_BIT,
+			OPERATION_PROGRESS_READ_WAIT,
+			OPERATION_PROGRESS_READ_END
+		} operationProgress = OPERATION_PROGRESS_IDLE;   
 
-        enum {
-            IDLE,
-            CHECK_FREE,
-            WORK,
-            WAITING,
-            CLEAR,
-            FINISH
-        } operationState = IDLE;
+		enum {
+			IDLE,
+			CHECK_FREE,
+			WORK,
+			WAITING,
+			CLEAR,
+			FINISH
+		} operationState = IDLE;
 
-        enum EoperationType {
-            RESET,
-            RECEIVE,
-            TRANSMIT
-        };
-            
-        struct operation {
-            EoperationType  operationType;
-            uint8_t         *pData;
-            uint16_t        Size;
-            bool            free = false;
-            dataCallback_f  callback_f = nullptr;
-        } currentOperation;
-        
-        std::queue<operation> operations;
+		enum EoperationType {
+			RESET,
+			RECEIVE,
+			TRANSMIT
+		};
+			
+		struct operation {
+			EoperationType  operationType;
+			uint8_t         *pData;
+			uint16_t        Size;
+			bool            free = false;
+			dataCallback_f  callback_f = nullptr;
+		} currentOperation;
+		
+		std::queue<operation> operations;
 };
 
 #endif // ONEWIRE_H
