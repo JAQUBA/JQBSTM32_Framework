f95497310c0bfdc2b77afda8e772b6cf4b37dc12
Karol Jakubowski <kjakubowski0492@gmail.com>
2025-06-06 19:32:47 +0200

    Dodaj wsparcie dla filtrowania i uśredniania w klasie Analog oraz popraw zarządzanie buforami DMA

--- src/Hardware/Analog/Analog.cpp
+++ src/Hardware/Analog/Analog.cpp
@@ -1,12 +1,37 @@
 #include "Analog.h"
 #ifdef __ANALOG_H_
 
+// Definicje dla filtrowania i uśredniania
+#ifndef ADC_SAMPLES_COUNT
+#define ADC_SAMPLES_COUNT 64    // Liczba próbek do uśredniania
+#endif
+
+#ifndef ADC_FILTER_ALPHA
+#define ADC_FILTER_ALPHA 0.1f   // Współczynnik filtra dolnoprzepustowego (0-1)
+#endif
+
+#ifndef ADC_DMA_BUFFER_SIZE
+#define ADC_DMA_BUFFER_SIZE 2   // Rozmiar bufora DMA (podwójny bufor)
+#endif
+
+#ifndef ADC_DEFAULT_MULTIPLIER
+#define ADC_DEFAULT_MULTIPLIER 1000  // Domyślny mnożnik (1.0 w fixed point)
+#endif
+
+#ifndef ADC_PROCESS_TASK_PERIOD
+#define ADC_PROCESS_TASK_PERIOD 10   // Okres zadania przetwarzania danych [ms]
+#endif
+
 Analog *_Analog_instances[ANALOG_MAX_INSTANCES];
 uint8_t _Analog_instancesNum = 0;
 
-uint16_t Analog::adcRAW[8];
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
+    Analog::getInstance(hadc)->convCpltCallback();
+}
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {Analog::getInstance(hadc)->convCpltCallback();}
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc) {
+    Analog::getInstance(hadc)->convHalfCpltCallback();
+}
 
 Analog* Analog::getInstance(ADC_HandleTypeDef *pHandler) {
     for (size_t i = 0; i < _Analog_instancesNum; i++) {
@@ -15,52 +40,140 @@ Analog* Analog::getInstance(ADC_HandleTypeDef *pHandler) {
     return nullptr;
 }
 
-Analog::Analog(ADC_HandleTypeDef *pHandler) : _pHandler(pHandler), bufferSize(pHandler->Init.NbrOfConversion) {
+Analog::Analog(ADC_HandleTypeDef *pHandler) : 
+    _pHandler(pHandler), 
+    bufferSize(pHandler->Init.NbrOfConversion),
+    sampleIndex(0),
+    dataReady(false) {
+    
     _Analog_instances[_Analog_instancesNum++] = this;
 
+    // Alokuj bufory DMA (podwójny bufor dla ciągłej konwersji)
+    adcDMABuffer = new uint32_t[bufferSize * ADC_DMA_BUFFER_SIZE];
     
-
-    // adcRAW = new uint32_t[bufferSize];
-
-    adcBuffer = new uint32_t[bufferSize];
-
-    adcValue = new uint32_t[bufferSize];
-
+    // Bufory dla próbek i uśredniania
+    adcSamples = new uint32_t*[bufferSize];
+    for (size_t i = 0; i < bufferSize; i++) {
+        adcSamples[i] = new uint32_t[ADC_SAMPLES_COUNT];
+        memset(adcSamples[i], 0, ADC_SAMPLES_COUNT * sizeof(uint32_t));
+    }
+    
+    // Bufory dla wyników
+    adcAverage = new uint32_t[bufferSize];
+    adcFiltered = new float[bufferSize];
+    adcValue = new uint16_t[bufferSize];
+    
+    // Konfiguracja kalibracji
     offsets = new uint16_t[bufferSize];
     multipliers = new uint16_t[bufferSize];
+    
+    // Inicjalizacja wartości domyślnych
+    for (size_t i = 0; i < bufferSize; i++) {
+        adcAverage[i] = 0;
+        adcFiltered[i] = 0.0f;
+        adcValue[i] = 0;
+        offsets[i] = 0;
+        multipliers[i] = ADC_DEFAULT_MULTIPLIER;
+    }
 
+    // Kalibracja ADC
     HAL_ADCEx_Calibration_Start(pHandler);
 
-    if (HAL_ADC_Start_DMA(pHandler, adcBuffer, bufferSize) != HAL_OK) {
+    // Start DMA w trybie cyklicznym
+    if (HAL_ADC_Start_DMA(pHandler, adcDMABuffer, bufferSize * ADC_DMA_BUFFER_SIZE) != HAL_OK) {
         Error_Handler();
     }
+    
+    // Zadanie przetwarzania danych
     addTaskMain(taskCallback {
-        for (size_t i = 0; i < bufferSize; i++) {
-            // adcValue[i] = ((adcBuffer[i] / adcSamplesNum));
-            // adcBuffer[i] = 0;
+        if (dataReady) {
+            processData();
+            dataReady = false;
+            display.toggle(displayMap::S3);
         }
-        // adcSamplesNum = 0;
-        display.toggle(displayMap::S3);//, true);
-    }, 1000);
+    }, ADC_PROCESS_TASK_PERIOD);
 }
 void Analog::convCpltCallback() {
-    display.toggle(displayMap::S1);//, true);
-    for (size_t i = 0; i < bufferSize; i++) {
-        // adcBuffer[i] = adcRAW[i];
-        // adcSamplesNum++;
+    // Przetwórz drugą połowę bufora DMA
+    processDMAData(&adcDMABuffer[bufferSize]);
+    display.toggle(displayMap::S1);
+}
+
+void Analog::convHalfCpltCallback() {
+    // Przetwórz pierwszą połowę bufora DMA
+    processDMAData(&adcDMABuffer[0]);
+    display.toggle(displayMap::S2);
+}
+
+void Analog::processDMAData(uint32_t* data) {
+    // Skopiuj nowe dane do bufora próbek
+    for (size_t channel = 0; channel < bufferSize; channel++) {
+        adcSamples[channel][sampleIndex] = data[channel];
+    }
+    
+    // Zwiększ indeks próbki
+    sampleIndex = (sampleIndex + 1) % ADC_SAMPLES_COUNT;
+    
+    // Oznacz że dane są gotowe do przetworzenia
+    dataReady = true;
+}
+
+void Analog::processData() {
+    for (size_t channel = 0; channel < bufferSize; channel++) {
+        // Oblicz średnią z próbek
+        uint64_t sum = 0;
+        for (size_t i = 0; i < ADC_SAMPLES_COUNT; i++) {
+            sum += adcSamples[channel][i];
+        }
+        adcAverage[channel] = sum / ADC_SAMPLES_COUNT;
+        
+        // Zastosuj filtr dolnoprzepustowy
+        adcFiltered[channel] = adcFiltered[channel] * (1.0f - ADC_FILTER_ALPHA) + 
+                              adcAverage[channel] * ADC_FILTER_ALPHA;
+        
+        // Zastosuj kalibrację i konwersję do końcowej wartości
+        uint32_t calibrated = ((uint32_t)adcFiltered[channel] + offsets[channel]) * multipliers[channel] / 1000;
+        
+        // Ogranicz do 16-bit
+        adcValue[channel] = (calibrated > 65535) ? 65535 : (uint16_t)calibrated;
     }
 }
 
 uint16_t Analog::getValue(uint8_t channel) {
-    // if (channel < bufferSize) {
-        return 200;
-    // }
-    // return 0;
-}
-void Analog::configureChannel(uint8_t channel, uint16_t *offset, uint16_t *multiplier) {
     if (channel < bufferSize) {
-        offsets[channel] = *offset;
-        multipliers[channel] = *multiplier;
+        return adcValue[channel];
+    }
+    return 0;
+}
+
+uint16_t Analog::getRawValue(uint8_t channel) {
+    if (channel < bufferSize) {
+        return (uint16_t)adcAverage[channel];
+    }
+    return 0;
+}
+
+float Analog::getFilteredValue(uint8_t channel) {
+    if (channel < bufferSize) {
+        return adcFiltered[channel];
+    }
+    return 0.0f;
+}
+
+void Analog::configureChannel(uint8_t channel, uint16_t offset, uint16_t multiplier) {
+    if (channel < bufferSize) {
+        offsets[channel] = offset;
+        multipliers[channel] = multiplier;
+    }
+}
+
+bool Analog::isDataReady() {
+    return !dataReady; // Zwraca true gdy dane nie są przetwarzane
+}
+
+void Analog::resetFilters() {
+    for (size_t i = 0; i < bufferSize; i++) {
+        adcFiltered[i] = adcAverage[i];
     }
 }
 #endif
--- src/Hardware/Analog/Analog.h
+++ src/Hardware/Analog/Analog.h
@@ -8,30 +8,67 @@
 #define ANALOG_MAX_INSTANCES 1
 #endif
 
+// Definicje dla filtrowania i uśredniania
+#ifndef ADC_SAMPLES_COUNT
+#define ADC_SAMPLES_COUNT 64    // Liczba próbek do uśredniania
+#endif
+
+#ifndef ADC_FILTER_ALPHA
+#define ADC_FILTER_ALPHA 0.1f   // Współczynnik filtra dolnoprzepustowego (0-1)
+#endif
+
+#ifndef ADC_DMA_BUFFER_SIZE
+#define ADC_DMA_BUFFER_SIZE 2   // Rozmiar bufora DMA (podwójny bufor)
+#endif
+
+#ifndef ADC_DEFAULT_MULTIPLIER
+#define ADC_DEFAULT_MULTIPLIER 1000  // Domyślny mnożnik (1.0 w fixed point)
+#endif
+
+#ifndef ADC_PROCESS_TASK_PERIOD
+#define ADC_PROCESS_TASK_PERIOD 10   // Okres zadania przetwarzania danych [ms]
+#endif
+
 #include "Application/Display/Display.h"
 extern Display display;
 
 class Analog {
-    public:
-        static Analog* getInstance(ADC_HandleTypeDef *pHandler);
-        Analog(ADC_HandleTypeDef *pHandler);
-        void configureChannel(uint8_t channel, uint16_t *offset, uint16_t *multiplier);
+private:
+    ADC_HandleTypeDef *_pHandler;
+    size_t bufferSize;
+    
+    // Bufory DMA i przetwarzania
+    uint32_t *adcDMABuffer;        // Podwójny bufor DMA
+    uint32_t **adcSamples;         // Bufor próbek dla każdego kanału
+    uint32_t *adcAverage;          // Uśrednione wartości
+    float *adcFiltered;            // Wartości po filtracji
+    uint16_t *adcValue;            // Końcowe wartości
+    
+    // Kalibracja
+    uint16_t *offsets;
+    uint16_t *multipliers;
+    
+    // Kontrola przetwarzania
+    volatile size_t sampleIndex;
+    volatile bool dataReady;
+    
+    void processDMAData(uint32_t* data);
+    void processData();
 
-        uint16_t getValue(uint8_t channel);
-        
-        void convCpltCallback();
-    private:
-        ADC_HandleTypeDef *_pHandler;
-        uint32_t bufferSize;
-        static uint16_t adcRAW[8];
-
-        uint32_t *adcBuffer;
-        uint16_t adcSamplesNum;
-
-        uint32_t *adcValue;
-
-        uint16_t *offsets;
-        uint16_t *multipliers;
+public:
+    static Analog* getInstance(ADC_HandleTypeDef *pHandler);
+    Analog(ADC_HandleTypeDef *pHandler);
+    
+    void convCpltCallback();
+    void convHalfCpltCallback();
+    
+    uint16_t getValue(uint8_t channel);
+    uint16_t getRawValue(uint8_t channel);
+    float getFilteredValue(uint8_t channel);
+    
+    void configureChannel(uint8_t channel, uint16_t offset, uint16_t multiplier);
+    bool isDataReady();
+    void resetFilters();
 };
 
 #endif
